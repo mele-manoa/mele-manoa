2316cd389c23dd5ac7375f470bf641d275dcf4fe
Isabelle <87943193+IGraceH@users.noreply.github.com>
2023-04-11 13:00:59 -1000

    changed text

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -45,7 +45,15 @@ const Landing = () => {
       <Container fluid id="info" className="mt-5">
         <Container className="py-5">
           <h1>What is Mele Manoa?</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <p>
+            Mele Manoa is a web application designed to bring musicians of UH together for jam sessions or
+            performing bands. It is an easy way for them to find others with similar tastes and compatible musical
+            abilities, by allowing students to create a profile indicating their musical tastes, their musical
+            capabilities, and their musical goals.
+          </p>
+          <p>
+            Simply log in, create your profile, and look for other musicians to jam with!
+          </p>
         </Container>
       </Container>
     </Container>

670c26271f975a551b611d91f019c4da5e5354b0
Isabelle <87943193+IGraceH@users.noreply.github.com>
2023-04-12 22:30:58 -1000

    basic mockup

--- app/imports/ui/pages/Discover.jsx
+++ app/imports/ui/pages/Discover.jsx
@@ -3,7 +3,24 @@ import { Container } from 'react-bootstrap';
 
 const Discover = () => {
   return (
-    <Container />
+    <Container id="discover" className="d-flex bg-white p-5">
+      <div id="discover-main" className="p-3">
+        <h1>Discover</h1>
+        <div id="discover-cards">
+          <p>
+            All the musicians go here
+          </p>
+        </div>
+      </div>
+      <div id="discover-sidebar" className="p-3">
+        <p>Filter By</p>
+        <ul>
+          <li>Instruments</li>
+          <ul>
+          </ul>
+        </ul>
+      </div>
+    </Container>
   );
 };
 

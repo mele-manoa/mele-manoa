ca272bf85a7ac117bc2a793f71d99a90ac5c4979
Isabelle <87943193+IGraceH@users.noreply.github.com>
2023-04-13 11:09:59 -1000

    profile mockup

--- app/imports/ui/pages/Profile.jsx
+++ app/imports/ui/pages/Profile.jsx
@@ -2,16 +2,21 @@ import React from 'react';
 import { Card, Col, Container, Row } from 'react-bootstrap';
 
 const Profile = () => (
-  <Container id="profile" className="d-flex bg-white p-6">
+  <Container id="profile" className="bg-white p-5">
     <Row className="justify-content-center">
-      <Col xs={5}>
-        <Col className="text-center py-4">
-          <h2>Profile</h2>
-        </Col>
-        <Card>
-          <Card.Body>
-          </Card.Body>
-        </Card>
+      <Col>
+        <h1>Your Profile</h1>
+        <ul>
+          <li>Name</li>
+          <li>Instrument</li>
+          <li>Genre</li>
+          <li>Skill</li>
+          <li>Open to Informal Jam</li>
+          <li>Seeking a band</li>
+        </ul>
+      </Col>
+      <Col>
+        Image Goes here
       </Col>
     </Row>
   </Container>

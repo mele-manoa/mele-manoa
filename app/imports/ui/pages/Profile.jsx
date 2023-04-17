fa7afa094b236450c5a12f1b03b225c06ad3d73e
Isabelle <87943193+IGraceH@users.noreply.github.com>
2023-04-16 17:36:43 -1000

    restructured page

--- app/imports/ui/pages/Profile.jsx
+++ app/imports/ui/pages/Profile.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Col, Container, Row, Image, Badge, Button } from 'react-bootstrap';
+import { Card, Container, Row, Col, Image, Badge, Button } from 'react-bootstrap';
 import { CheckLg, XLg } from 'react-bootstrap-icons';
 
 const Profile = () => {
@@ -20,49 +20,53 @@ const Profile = () => {
   return (
     <Container id="profile" className="bg-white p-5">
       <h1>Profile</h1>
-      <Card id="profile-card">
-        <Card.Body className="d-flex">
-          <div className="me-auto">
-            <Card.Title>{info.name}</Card.Title>
-            <Card.Title><Badge bg="secondary">{info.instrument}</Badge></Card.Title>
-            <Card.Text>
-              Preferred Genre: {info.genre} <br />
-              Skill Level: {info.skill}
-            </Card.Text>
-            <Card.Text>
-              { info.informalJam === true ? (
-                <Badge bg="success" className="me-1"><CheckLg /></Badge>
-              ) : (
-                <Badge bg="warning" className="me-1"><XLg /></Badge>
-              )}
-              Open to Informal Jam <br />
-              { info.seekingBand === true ? (
-                <Badge bg="success" className="me-1"><CheckLg /></Badge>
-              ) : (
-                <Badge bg="danger" className="me-1"><XLg /></Badge>
-              )}
-              Seeking a band
-            </Card.Text>
-          </div>
-          <Image id="profile-image" className="float-right" src={info.image} thumbnail />
-        </Card.Body>
-        <Card.Footer className="d-flex">
-          <div className="me-auto">
-            { [info.youtube, info.soundcloud, info.instagram].map((link) => (
-              link !== '' || null ? (
-                <Button href={link} className="on-white me-1" variant="secondary">
-                  { link === info.youtube ? 'Youtube' : '' }
-                  { link === info.soundcloud ? 'SoundCloud' : '' }
-                  { link === info.instagram ? 'Instagram' : '' }
-                </Button>
-              ) : ''
-            ))}
-          </div>
-          <Button className="blue on-white">Edit</Button>
-        </Card.Footer>
-      </Card>
       <Row>
-        <h3>Groups</h3>
+        <Col>
+          <Card id="profile-card">
+            <Card.Body className="d-flex">
+              <div className="me-auto">
+                <Card.Title>{info.name}</Card.Title>
+                <Card.Title><Badge bg="secondary">{info.instrument}</Badge></Card.Title>
+                <Card.Text>
+                  Preferred Genre: {info.genre} <br />
+                  Skill Level: {info.skill}
+                </Card.Text>
+                <Card.Text>
+                  { info.informalJam === true ? (
+                    <Badge bg="success" className="me-1"><CheckLg /></Badge>
+                  ) : (
+                    <Badge bg="warning" className="me-1"><XLg /></Badge>
+                  )}
+                  Open to Informal Jam <br />
+                  { info.seekingBand === true ? (
+                    <Badge bg="success" className="me-1"><CheckLg /></Badge>
+                  ) : (
+                    <Badge bg="danger" className="me-1"><XLg /></Badge>
+                  )}
+                  Seeking a band
+                </Card.Text>
+              </div>
+              <Image id="profile-image" className="float-right" src={info.image} thumbnail />
+            </Card.Body>
+            <Card.Footer className="d-flex">
+              <div className="me-auto">
+                { [info.youtube, info.soundcloud, info.instagram].map((link) => (
+                  link !== '' || null ? (
+                    <Button href={link} className="on-white me-1" variant="secondary">
+                      { link === info.youtube ? 'Youtube' : '' }
+                      { link === info.soundcloud ? 'SoundCloud' : '' }
+                      { link === info.instagram ? 'Instagram' : '' }
+                    </Button>
+                  ) : ''
+                ))}
+              </div>
+              <Button className="blue on-white">Edit</Button>
+            </Card.Footer>
+          </Card>
+        </Col>
+        <Col>
+          <h3>Groups</h3>
+        </Col>
       </Row>
     </Container>
   );

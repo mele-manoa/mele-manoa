7fb4d26e3d7d6cadd3fd6f8589b4afa9ae91e414
Isabelle <87943193+IGraceH@users.noreply.github.com>
2023-04-17 20:05:21 -1000

    accordion

--- app/imports/ui/pages/Groups.jsx
+++ app/imports/ui/pages/Groups.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, ListGroup } from 'react-bootstrap';
+import { Container, Accordion, ListGroup } from 'react-bootstrap';
 // import UserCard from '../components/UserCard';
 
 const Groups = () => {
@@ -11,26 +11,32 @@ const Groups = () => {
         <div id="groups-cards" className="d-flex" />
       </div>
       <div id="groups-sidebar" className="p-3">
-        <h2>Filter By</h2>
-        <h4>Genres</h4>
-        <ListGroup>
-          <ListGroup.Item>Rock</ListGroup.Item>
-          <ListGroup.Item>Jazz</ListGroup.Item>
-          <ListGroup.Item>EDM</ListGroup.Item>
-          <ListGroup.Item>Dubstep</ListGroup.Item>
-          <ListGroup.Item>Country</ListGroup.Item>
-          <ListGroup.Item>Pop</ListGroup.Item>
-          <ListGroup.Item>Classical</ListGroup.Item>
-          <ListGroup.Item>RhythmAndBlues</ListGroup.Item>
-        </ListGroup>
-        <h4>Skill Level</h4>
+        <h4>Filter By</h4>
+        <Accordion defaultActiveKey="0">
+          <Accordion.Item eventKey="0">
+            <Accordion.Header>Genres</Accordion.Header>
+            <Accordion.Body>
+              <ListGroup className="m-0">
+                <ListGroup.Item>Rock</ListGroup.Item>
+                <ListGroup.Item>Jazz</ListGroup.Item>
+                <ListGroup.Item>EDM</ListGroup.Item>
+                <ListGroup.Item>Dubstep</ListGroup.Item>
+                <ListGroup.Item>Country</ListGroup.Item>
+                <ListGroup.Item>Pop</ListGroup.Item>
+                <ListGroup.Item>Classical</ListGroup.Item>
+                <ListGroup.Item>Rhythm And Blues</ListGroup.Item>
+              </ListGroup>
+            </Accordion.Body>
+          </Accordion.Item>
+        </Accordion>
+        <h5>Skill Level</h5>
         <ListGroup>
           <ListGroup.Item>Beginner</ListGroup.Item>
           <ListGroup.Item>Intermediate</ListGroup.Item>
           <ListGroup.Item>Expert</ListGroup.Item>
           <ListGroup.Item>Professional</ListGroup.Item>
         </ListGroup>
-        <h4>Seeking Band Member</h4>
+        <h5>Seeking Band Member</h5>
       </div>
     </Container>
   );

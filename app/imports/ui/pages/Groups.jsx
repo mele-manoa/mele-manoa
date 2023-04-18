b89fe1cc7e96d325d07bf60a42349f96652b9882
Isabelle <87943193+IGraceH@users.noreply.github.com>
2023-04-17 19:57:06 -1000

    changed overall text color

--- app/imports/ui/pages/Groups.jsx
+++ app/imports/ui/pages/Groups.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, ListGroup } from 'react-bootstrap';
 // import UserCard from '../components/UserCard';
 
 const Groups = () => {
@@ -11,28 +11,26 @@ const Groups = () => {
         <div id="groups-cards" className="d-flex" />
       </div>
       <div id="groups-sidebar" className="p-3">
-        <p>Filter By</p>
-        <ul>
-          <li>Genres</li>
-          <ul>
-            <li>Rock</li>
-            <li>Jazz</li>
-            <li>EDM</li>
-            <li>Dubstep</li>
-            <li>Country</li>
-            <li>Pop</li>
-            <li>Classical</li>
-            <li>RhythmAndBlues</li>
-          </ul>
-          <li>Skill Level</li>
-          <ul>
-            <li>Beginner</li>
-            <li>Intermediate</li>
-            <li>Expert</li>
-            <li>Professional</li>
-          </ul>
-          <li>Seeking Band Member</li>
-        </ul>
+        <h2>Filter By</h2>
+        <h4>Genres</h4>
+        <ListGroup>
+          <ListGroup.Item>Rock</ListGroup.Item>
+          <ListGroup.Item>Jazz</ListGroup.Item>
+          <ListGroup.Item>EDM</ListGroup.Item>
+          <ListGroup.Item>Dubstep</ListGroup.Item>
+          <ListGroup.Item>Country</ListGroup.Item>
+          <ListGroup.Item>Pop</ListGroup.Item>
+          <ListGroup.Item>Classical</ListGroup.Item>
+          <ListGroup.Item>RhythmAndBlues</ListGroup.Item>
+        </ListGroup>
+        <h4>Skill Level</h4>
+        <ListGroup>
+          <ListGroup.Item>Beginner</ListGroup.Item>
+          <ListGroup.Item>Intermediate</ListGroup.Item>
+          <ListGroup.Item>Expert</ListGroup.Item>
+          <ListGroup.Item>Professional</ListGroup.Item>
+        </ListGroup>
+        <h4>Seeking Band Member</h4>
       </div>
     </Container>
   );

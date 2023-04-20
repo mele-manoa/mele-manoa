d9fb971a193afd0552c7a7904cb2d7610ad2b8cc
Yeeun Shin <yeeun2@hawaii.edu>
2023-04-20 13:28:04 -1000

    Edit profile page edited

--- app/imports/ui/pages/EditProfile.jsx
+++ app/imports/ui/pages/EditProfile.jsx
@@ -3,78 +3,46 @@ import { Card, Col, Container, Row } from 'react-bootstrap';
 import { AutoForm, ErrorsField, HiddenField, TextField, SubmitField } from 'uniforms-bootstrap5';
 import swal from 'sweetalert';
 import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
-import SimpleSchema from 'simpl-schema';
 import { People } from '../../api/people/People';
 
-const formSchema = new SimpleSchema({
-  email: String,
-  name: String,
-  image: String,
-  instrument: {
-    type: String,
-    allowedValues: String,
-    defaultValue: 0,
-  },
-  genre: {
-    type: String,
-    allowedValues: String,
-    defaultValue: 0,
-  },
-  skill: {
-    type: String,
-    allowedValues: String,
-    defaultValue: 0,
-  },
-  informalJam: Boolean,
-  seekingBand: Boolean,
-  youtube: String,
-  soundcloud: String,
-  instagram: String,
-});
-
-const bridge = new SimpleSchema2Bridge(formSchema);
+const bridge = new SimpleSchema2Bridge(People.schema);
 
 /* Renders the EditStuff page for editing a single document. */
 const EditProfile = () => {
   const submit = (data) => {
-    const { email, name, image, instrument, genre, skill, informalJam, seekingband, youtube, soundcloud, instagram } = data;
+    const { email, name, image, instrument, genre, skill } = data;
     People.collection.insert(
-      { email, name, image, instrument, genre, skill, informalJam, seekingband, youtube, soundcloud, instagram },
+      { email, name, image, instrument, genre, skill },
       (error) => {
         if (error) {
           swal('Error', error.message, 'error');
         } else {
-          swal('Success');
+          swal('Success', 'Item updated successfully', 'success');
         }
       },
     );
   };
 
-  const fRef = null;
+  // Render the form. Use Uniforms: https://github.com/vazco/uniforms
+  let fRef = null;
   return (
     <Container className="py-3">
       <Row className="justify-content-center">
-        <Col xs={10}>
+        <Col xs={8}>
           <Col className="text-center"><h2>Edit Profile</h2></Col>
           <AutoForm ref={ref => { fRef = ref; }} schema={bridge} onSubmit={data => submit(data, fRef)}>
             <Card>
               <Card.Body>
                 <Row>
+                  <Col><TextField name="Email" /></Col>
                   <Col><TextField name="Name" /></Col>
                 </Row>
                 <Row>
-                  <Col><TextField name="email" /></Col>
-                </Row>
-                <Row>
                   <Col><TextField name="instrument" /></Col>
-                </Row>
-                <Row>
                   <Col><TextField name="image" /></Col>
                 </Row>
                 <Row>
                   <Col><TextField name="genre" /></Col>
-                </Row>
-                <Row>
                   <Col><TextField name="skill" /></Col>
                 </Row>
                 <SubmitField value="Submit" />

5e69a473262fd9200a81579925ed1de7d27dca12
Yeeun Shin <yeeun2@hawaii.edu>
2023-04-29 19:59:23 -1000

    created register profile page

--- app/imports/ui/pages/EditProfile.jsx
+++ app/imports/ui/pages/EditProfile.jsx
@@ -1,46 +1,69 @@
 import React from 'react';
-import { Card, Col, Container, Row, Button } from 'react-bootstrap';
-import { AutoForm, ErrorsField, TextField, SubmitField, SelectField, BoolField } from 'uniforms-bootstrap5';
+import { Meteor } from 'meteor/meteor';
+import { AutoForm, TextField, SelectField, SubmitField, BoolField, ErrorsField } from 'uniforms-bootstrap5';
+import { Container, Col, Card, Row, Button } from 'react-bootstrap';
+import swal from 'sweetalert';
 import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
 import SimpleSchema from 'simpl-schema';
+import { useTracker } from 'meteor/react-meteor-data';
+import { People } from '../../api/people/People';
+import LoadingSpinner from '../components/LoadingSpinner';
+
+const schema = new SimpleSchema({
+  name: String,
+  image: String,
+  instrument: String,
+  genre: {
+    type: String,
+    allowedValues: ['Rock', 'Jazz', 'EDM', 'Dubstep', 'Country', 'Pop', 'Classical', 'RhythmAndBlues'],
+    defaultValue: 'Rock',
+  },
+  skill: {
+    type: String,
+    allowedValues: ['Beginner', 'Intermediate', 'Expert', 'Professional'],
+    defaultValue: 'Beginner',
+  },
+  informalJam: {
+    type: Boolean,
+    allowedValues: [true, false],
+    defaultValue: false,
+  },
+  seekingBand: {
+    type: Boolean,
+    allowedValues: [true, false],
+    defaultValue: false,
+  },
+  youtube: { type: String, optional: true },
+  soundcloud: { type: String, optional: true },
+  instagram: { type: String, optional: true },
+});
+
+const bridge = new SimpleSchema2Bridge(schema);
 
 /* Renders the EditStuff page for editing a single document. */
 const EditProfile = () => {
-  const schema = new SimpleSchema({
-    name: String,
-    image: String,
-    instrument: String,
-    genre: {
-      type: String,
-      allowedValues: ['Rock', 'Jazz', 'EDM', 'Dubstep', 'Country', 'Pop', 'Classical', 'RhythmAndBlues'],
-      defaultValue: 'Rock',
-    },
-    skill: {
-      type: String,
-      allowedValues: ['Beginner', 'Intermediate', 'Expert', 'Professional'],
-      defaultValue: 'Beginner',
-    },
-    informalJam: {
-      type: Boolean,
-      allowedValues: [true, false],
-      defaultValue: false,
-    },
-    seekingBand: {
-      type: Boolean,
-      allowedValues: [true, false],
-      defaultValue: false,
-    },
-    youtube: { type: String, optional: true },
-    soundcloud: { type: String, optional: true },
-    instagram: { type: String, optional: true },
-  });
-  const bridge = new SimpleSchema2Bridge(schema);
-  return (
+
+  const { ready, email } = useTracker(() => {
+    // Ensure that minimongo is populated with all collections prior to running render().
+    const sub = Meteor.subscribe(People.userPublicationName);
+    return {
+      ready: sub.ready(),
+      email: Meteor.user()?.username,
+    };
+  }, []);
+  const submit = (data) => {
+    const { _id, name, image, instrument, genre, skill, informalJam, seekingBand, youtube, soundcloud, instagram } = data;
+    People.collection.update(_id, { $set: { name, image, instrument, genre, skill, informalJam, seekingBand, youtube, soundcloud, instagram } }, (error) => (error ?
+      swal('Error', error.message, 'error') :
+      swal('Success', 'Item updated successfully', 'success')));
+  };
+  const info = People.collection.findOne({ email });
+  return ready ? (
     <Container id="edit-profile" className="bg-white p-5">
       <Row className="justify-content-center">
         <Col xs={10}>
           <Col><h2>Edit Profile</h2></Col>
-          <AutoForm schema={bridge} onSubmit={console.log('submit')}>
+          <AutoForm model={info} schema={bridge} onSubmit={data => submit(data)}>
             <Card>
               <Card.Body>
                 <Row>
@@ -78,7 +101,7 @@ const EditProfile = () => {
         </Col>
       </Row>
     </Container>
-  );
+  ) : <LoadingSpinner />;
 };
 
 export default EditProfile;

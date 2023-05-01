c42c1d4eb197287608927d2082f392f0f26e1029
Isabelle G Hu <87943193+IGraceH@users.noreply.github.com>
2023-05-01 09:32:38 -1000

    Revert "Issue 54 - Clean up style.css file"

--- app/imports/ui/components/UserCard.jsx
+++ app/imports/ui/components/UserCard.jsx
@@ -27,7 +27,9 @@ const UserCard = ({ info }) => (
     </Card.Body>
     <Card.Footer>
       { [info.youtube, info.soundcloud, info.instagram].map((link) => (
-        (link !== undefined) ? (
+        (link !== '') || (link !== null) || (link !== undefined) ? (
+          ''
+        ) : (
           <OverlayTrigger
             overlay={(
               <Tooltip>
@@ -43,8 +45,6 @@ const UserCard = ({ info }) => (
               { link === info.instagram ? (<Instagram />) : '' }
             </Button>
           </OverlayTrigger>
-        ) : (
-          ''
         )
       ))}
     </Card.Footer>

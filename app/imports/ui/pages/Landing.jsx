f5cc8b0ae1ffce91efbc8e4fc396ff6d5ff5522d
Isabelle <87943193+IGraceH@users.noreply.github.com>
2023-05-02 16:35:20 -1000

    updated user guide

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { useTracker } from 'meteor/react-meteor-data';
-import { Button, Col, Container, Row, Nav, Image } from 'react-bootstrap';
+import { Button, Col, Container, Row, Nav, Image, Stack } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 
 /* A simple static component to render some text for the landing page. */
@@ -45,6 +45,11 @@ const Landing = () => {
       <Container fluid id="landing-info" className="mt-5">
         <Container className="py-5">
           <h1>What is Mele Manoa?</h1>
+          <Stack className="figure float-end ps-4">
+            <Image src="/images/userguide/userGuide6.png" rounded thumbnail />
+            <Image src="/images/userguide/userGuide7.png" rounded thumbnail />
+            <Image src="/images/userguide/userGuide8.png" rounded thumbnail />
+          </Stack>
           <p>
             Mele Manoa is a web application designed to bring musicians of UH together for jam sessions or performing
             bands. It is an easy way for them to find others with similar tastes and compatible musical abilities, by
@@ -58,17 +63,10 @@ const Landing = () => {
           </p>
           <h3>User Guide</h3>
           <p>
-            Welcome to Mele Manoa! You can sign in or register using any of the buttons on the page. The sign in and
-            register buttons will allow you to login into your account to start using Mele Manoa. If this is your first
+            If this is your first
             time here, after registration you will also be prompted to fill in some information about yourself. This
             will allow other musicians of UH to find out about you and see who you are on Mele Manoa.
           </p>
-          <figure className="figure float-end ps-4">
-            <Image src="/images/userguide/userGuide2.png" className="rounded" />
-          </figure>
-          <figure className="figure float-end ps-4">
-            <Image src="/images/userguide/userGuide3.png" className="rounded" />
-          </figure>
           <p>
             All this information will also appear on your profile, which you can edit at any time. You can put in your
             name, what instrument you play, an image to represent you, what your preferred genre is, and your skill
@@ -76,48 +74,26 @@ const Landing = () => {
             check: if you are open to informal jams with other musicians, or if you are seeking a band to join. You are
             also able to add in external social media links to Youtube, SoundCloud, or Instagram to help others
             understand what kind of music you make. Just make sure the link starts with `https://`!
-
+            <br /><br />
             Once you hit submit, you will find your profile all set and ready for you! If you wish to change your
-            information, simply click the edit button once more and you can edit your information all you want. In this
-            case, I wanted to change my preferred genre and my skill level. I also added in my SoundCloud link.*
+            information, simply click the edit button once more and you can edit your information all you want.
           </p>
-          <figure className="figure float-end ps-4">
-            <Image src="/images/userguide/userGuide4.png" className="rounded" />
-          </figure>
-          <figure className="figure float-end ps-4">
-            <Image src="/images/userguide/userGuide5.png" className="rounded" />
-            <figcaption className="figure-caption text-wrap" style={{ width: '500px' }}>Note: External links are not real and for demonstration purposes only. </figcaption>
-          </figure>
           <p>
             When you think your profile looks good, head over to the <b>Discover</b> page on the navigation bar. Here you
             will find all your fellow musicians who also use Mele Manoa. You can filter through them using the sidebar
             on the right, depending on what kind of musicians you’re looking for to jam with! The <b>Discover</b> page
             gives you all the information you need about them.
           </p>
-          <figure className="figure float-end ps-4">
-            <Image src="/images/userguide/userGuide6.png" className="rounded" />
-          </figure>
           <p>
-            Then the <b>Groups</b> page, this page is for musical groups that have already formed within UH. Like in the
-            <b>Discover</b> page, you can also filter through the groups to find your favorites and perhaps one you may be
-            interested in joining.
+            Then the <b>Groups</b> page, this page is for musical groups that have already formed within UH.
+            Like in the <b>Discover</b> page, you can also filter through the groups to find your favorites and perhaps one you may be interested in joining.
           </p>
-          <figure className="figure float-end ps-4">
-            <Image src="/images/userguide/userGuide7.png" className="rounded" />
-          </figure>
           <p>
             For Admins, new buttons will show up on the <b>Groups</b> page to add new groups or edit existing groups.
             Similar to users, you can set the group’s name, image, members, genre, and if the group is open to new
             members or not. Members should be written in a comma-separated list to distinguish between each group
             member. Edit group is very similar, just press on a group’s edit button to edit their information.
           </p>
-          <figure className="figure float-end ps-4">
-            <Image src="/images/userguide/userGuide8.png" className="rounded" />
-          </figure>
-          <figure className="figure float-end ps-4">
-            <Image src="/images/userguide/userGuide9.png" className="rounded" />
-          </figure>
-
           <p>Thank you so much for using Mele Manoa!</p>
         </Container>
       </Container>
